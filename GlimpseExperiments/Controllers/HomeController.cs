5ca202309534fbdb466b14333a6157beae78cc9f
Steve Franceschelli <sfranceschelli@vistaprint.com>
2014-10-06 13:00:26 -0400

    Add session data for testing.

--- GlimpseExperiments/Controllers/HomeController.cs
+++ GlimpseExperiments/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace GlimpseExperiments.Controllers
     {
         public ActionResult Index()
         {
+            Session["testData"] = "test value";
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
