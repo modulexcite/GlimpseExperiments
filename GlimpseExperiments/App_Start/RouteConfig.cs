3d919d16adffbd9449dcd87f3fcef4a6633864e8
Steve Franceschelli <sfranceschelli@vistaprint.com>
2014-10-06 12:51:19 -0400

    Remove constraints and DataTokens.

--- GlimpseExperiments/App_Start/RouteConfig.cs
+++ GlimpseExperiments/App_Start/RouteConfig.cs
@@ -16,9 +16,8 @@ namespace GlimpseExperiments
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                constraints: new RouteValueDictionary { { "id", @"\d" } }
-            ).DataTokens = new RouteValueDictionary { { "data", "value" } };
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
