08835e4ec6d00d75123b55e8dacf98ec6ad25bb7
Steve Franceschelli <sfranceschelli@vistaprint.com>
2014-10-06 12:50:42 -0400

    Add constraints and DataTokens to RouteConfig.

--- GlimpseExperiments/App_Start/RouteConfig.cs
+++ GlimpseExperiments/App_Start/RouteConfig.cs
@@ -16,8 +16,9 @@ namespace GlimpseExperiments
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                constraints: new RouteValueDictionary { { "id", @"\d" } }
+            ).DataTokens = new RouteValueDictionary { { "data", "value" } };
         }
     }
 }
